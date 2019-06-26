73a8bd0e697cbb058501af8dd8255e46f5009019
dkumazaw <dkumazaw@stanford.edu>
2019-06-26 12:02:21 +0900

    Update

--- runtime-modeling/gen_tflite_models.sh
+++ runtime-modeling/gen_tflite_models.sh
@@ -4,12 +4,15 @@
 # k: kernel size (for all MBConvs)
 # e: expansio ratio (for all MBConvs)
 
+mkdir tflite-models
+
 d=100
 for k in 3 5
 do
-  for e in 3 6
+  for e in 2 3 4 5 6
   do	    
       echo $d, $k, $e
+      mkdir tflite-models/model-$d-$k-$e
       python main_tflite.py --tpu=$TPU_NAME --data_dir=$DATA_DIR --model_dir=${STORAGE_BUCKET}/model-runtime-model/model-tflite-$d-$k-$e --export_dir=$(pwd)/tflite-models/model-$d-$k-$e --depth_multiplier=$d --kernel=$k --expratio=$e --mode=train  --post_quantize=True
       python profiler_scripts.py $(pwd)/tflite-models/model-$d-$k-$e/
   done
